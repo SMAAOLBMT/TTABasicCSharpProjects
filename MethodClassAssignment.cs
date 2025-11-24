ce72d00dd343dfe019532d8f9baf46c6374a96c8
SMAAOLBMT <SyedMansoor.Ahmed@my-aolcc.com>
2025-11-24 11:26:52 -0500

    Add overloaded MathOp methods for decimals and strings
    Added two new MathOp methods for decimal and string inputs.

--- MethodClassAssignment.cs
+++ MethodClassAssignment.cs
@@ -5,19 +5,20 @@ namespace MethodClassAssignment
     public class MethodClassAssignment
     {
         public void MathOp(int number1, int number2)
-        {
-            int total = number1 + 50;
-            Console.WriteLine("new value for number1 is: " + total.ToString() + " AND old value of number2 was: " + number2.ToString());
-        }
-        /*public int MathOp(decimal number)
-        {
-            int newnum = Convert.ToInt32(number);
-            return newnum * 20;
-        }
-        public int MathOp(string number)
-        {
-            int newnum = Convert.ToInt32(number);
-            return newnum % 7;
-        }*/
+    {
+        int total = number1 + 50;
+        Console.WriteLine("new value for number1 is: " + total.ToString() + " AND old value of number2 was: " + number2.ToString());
     }
+    public decimal MathOp2(decimal number1, decimal number2)
+    {
+        decimal newnum = Convert.ToDecimal(number1);
+        return newnum * 50;
+    }
+    public int MathOp3(string number1, string number2)
+    {
+        int newnum = 50;
+        return newnum % Convert.ToInt32(number1);
+    }
+  }
+
 }
